07b7af8d9013733df0da437e295bb20cae9f3bf3
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-09-26 22:21:51 -0700

    Add about.js and links

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div>
+    <h2>This is the about page</h2>
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
+const linkStyle = {
+  fontSize: 20
+};
+
 const Index = () => (
   <div>
+    <Link href="/about">
+      {/* <a style={linkStyle}>About Page</a> */}
+      <button>Go to About Page</button>
+    </Link>
     <h2>Hello Next.js</h2>
   </div>
-)
+);
 
-export default Index
+export default Index;
