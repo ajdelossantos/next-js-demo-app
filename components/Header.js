e183b384f10ac1837c7304eb41486cc721cbf65f
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-09-26 22:59:53 -0700

    Add basic Header styling

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,22 @@
 import Link from 'next/link';
 
 const linkStyle = {
-  marginRight: 15
+  marginRight: 15,
+  padding: '5px 10px',
+  border: '1px solid blue',
+  borderRadius: 2,
+  textDecoration: 'none'
+};
+
+const titleStyle = {
+  display: 'flex',
+  padding: '5px 20px 20px 20px',
+  borderBottom: '1px solid #DDD',
+  justifyContent: 'flex-start'
 };
 
 const Header = () => (
-  <div>
+  <div style={titleStyle}>
     <Link href="/">
       <a style={linkStyle}>Home</a>
     </Link>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Link from 'next/link';
 import Layout from '../components/MyLayout';
 
+const titleStyle = {
+  textAlign: 'center',
+  fontSize: 36
+};
+
 const Index = () => (
   <div>
     <Layout>
-      <h2>Hello Next.js</h2>
+      <h2 style={titleStyle}>My Next.js Blog</h2>
+      <ul>
+        <li>This seems cool</li>
+        <li>Never mind, this app is garbage</li>
+        <li>10 Cool Tricks to Learning Web Development</li>
+      </ul>
     </Layout>
   </div>
 );
